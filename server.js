cde968e83faa4b999488c951e43e2abdb66017b6
Yogesh Late <yogesh.late@planetmarathi.com>
2023-04-25 14:00:32 +0530

    feat: mongo db setup and connecting express app to mongodb database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
+const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 
+//Connecting express app to mongodb database
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
