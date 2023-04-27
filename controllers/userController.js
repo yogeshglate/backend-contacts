e507170198bb1517275ed771d3309b70f2753f14
Yogesh Late <yogesh.late@planetmarathi.com>
2023-04-27 16:51:11 +0530

    feat: Protecting user routes & verify jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,7 +73,7 @@ const loginUser = asyncHandler(async (req, res) => {
 //@access private
 
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Get the User Information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
